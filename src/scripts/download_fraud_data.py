3de0a2caa30cd1e116bc0851768124a719750414
Jonathan Guymont <39598613+JGuymont@users.noreply.github.com>
2019-09-24 21:41:41 -0400

    Update download_fraud_data.py

--- src/scripts/download_fraud_data.py
+++ src/scripts/download_fraud_data.py
@@ -1,17 +1,16 @@
 import zipfile
 from google_drive_downloader import GoogleDriveDownloader
 
+#TRAIN_IDENTITY_ID = '1ablR0JkfExUWdvSWKZPO6ZRxsR_XRIIp'
+#TRAIN_TRANSACTION_ID = '1km69I5XBUT1MWjTvr0y514x6DdKpHUvF'
+#TRAIN_IDENTITY_ZIP_FILEPATH = './data/interim/train_identity.zip'
 
 DATA_DIR = './data/interim/'
 
-TRAIN_IDENTITY_ID = '1ablR0JkfExUWdvSWKZPO6ZRxsR_XRIIp'
-TRAIN_TRANSACTION_ID = '1km69I5XBUT1MWjTvr0y514x6DdKpHUvF'
-
+TRAIN_TRANSACTION_ID = '1W0zWmofwgn70wM8oZDAk_NWVO4VGeyr8'
 TEST_TRANSACTION_ID = '1V5oZtGscXneNHMX5mDcFut-y6AKVgIo_'
 
-TRAIN_IDENTITY_ZIP_FILEPATH = './data/interim/train_identity.zip'
 TRAIN_TRANSACTION_ZIP_FILEPATH = './data/interim/train_transaction.zip'
-
 TEST_TRANSACTION_ZIP_FILEPATH = './data/interim/test_transaction.zip'
 
 
@@ -27,6 +26,6 @@ def load_and_extract(file_id, zip_filepath, out_dir):
 
 
 if __name__ == '__main__':
-    load_and_extract(TRAIN_IDENTITY_ID, TRAIN_IDENTITY_ZIP_FILEPATH, DATA_DIR)
+    # load_and_extract(TRAIN_IDENTITY_ID, TRAIN_IDENTITY_ZIP_FILEPATH, DATA_DIR)
     load_and_extract(TRAIN_TRANSACTION_ID, TRAIN_TRANSACTION_ZIP_FILEPATH, DATA_DIR)
     load_and_extract(TEST_TRANSACTION_ID, TEST_TRANSACTION_ZIP_FILEPATH, DATA_DIR)
